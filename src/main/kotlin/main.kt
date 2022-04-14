5dc02283cb137b1db9040a64fb68c5e5fcc55fd4
mitchelle-hope <mitchellehopewaithera@gmail.com>
2022-04-14 09:27:32 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,50 @@
+fun main{
+    CurrentAccount(4567,"Nancy Drew",7000)
+        name("banana")
+
+}
+//Create a classCurrentAccount with the following attributes:account
+//number, account name, balance. It has the following functions:
+//a.deposit(amount: Double) - Increments the balance by the
+//amount deposited
+//b.withdraw(amount: Double) - Decrements the balance by the
+//amount withdrawn
+//c.details() - Prints out the account number and balance and
+//name in this format: “Account number x with b
+open class CurrentAccount(var Accountnum:Int,var Accountname:String,var Balance:Int):{
+   fun Deposit(Amount:Double){
+       var q = Amount + Balance
+       if (Deposit =+ Balance)
+           println(Balance)
+
+   }
+      Println (Deposit)
+}
+fun withdraw(Amount: Double){
+    var z = Balance - Amount
+    if (Amount- Balance)
+}
+fun Details(){
+
+    println("Account number $ with $")
+}
+/
+//}A product is represented by a data class with these attributes: name,
+//weight, price, category. Category can either be groceries, hygiene or
+class(name:String,weight:Int,category: String){
+    fun
+
+
+}
+fun word(name:String){
+
+}
+fun name(names: <String): Int{
+    var num = 0
+    for (name in names){
+        if (name.length>5){
+            num++
+        }
+    }
+    return num
+}
